8c33cc613c9f80e7b3eddefe01626801af4bd750
Romain AROLDI <romain.aroldi57@gmail.com>
2024-12-19 15:34:06 +0100

    Adapt utility functions following status update

--- src/utils/state.util.ts
+++ src/utils/state.util.ts
@@ -2,16 +2,29 @@ import type {State} from "../interfaces/state.interface.ts";
 import type {Team} from "../types/team.type.ts";
 
 export const updateState = (state: State, team: Team, scored: boolean): State => {
-    const newScoreA = team === "A" && scored ? state.scoreA + 1 : state.scoreA;
-    const newScoreB = team === "B" && scored ? state.scoreB + 1 : state.scoreB;
+    const newScores = {
+        A: team === "A" && scored ? state.scoreA + 1 : state.scoreA,
+        B: team === "B" && scored ? state.scoreB + 1 : state.scoreB,
+    };
+
     const shotResult = `${team === "A" ? "Équipe A" : "Équipe B"} : ${scored ? "+1" : "0"}`;
-    const newHistory = [...state.history, `Score : ${newScoreA}/${newScoreB} (${shotResult})`];
+    const newHistory = [
+        ...state.history,
+        `Tir ${state.totalShots - state.shotsRemaining + 1} : Score : ${newScores.A}/${newScores.B} (${shotResult})`,
+    ];
 
-    return {scoreA: newScoreA, scoreB: newScoreB, history: newHistory};
-}
+    return {
+        ...state,
+        scoreA: newScores.A,
+        scoreB: newScores.B,
+        history: newHistory,
+        shotsRemaining: state.shotsRemaining - 1,
+    };
+};
 
-export const checkWinner = (state: State, totalShots: number): string | null => {
-    const {scoreA, scoreB} = state;
+
+export const checkWinner = (state: State): string | null => {
+    const {scoreA, scoreB, totalShots} = state;
 
     if (scoreA > scoreB && scoreA - scoreB > totalShots / 2) return "Équipe A";
     if (scoreB > scoreA && scoreB - scoreA > totalShots / 2) return "Équipe B";
